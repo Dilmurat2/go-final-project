fd774b5468f1ea1b1253c5a8c8287a8df15f179e
Dilmurat <dlol96@mail.ru>
2024-05-19 22:45:15 +0500

    fix

--- menu/internal/config/config.go
+++ menu/internal/config/config.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"fmt"
-	"github.com/joho/godotenv"
 	"github.com/kelseyhightower/envconfig"
 )
 
@@ -19,9 +18,7 @@ type Config struct {
 
 func New() (*Config, error) {
 	cfg := new(Config)
-	if err := godotenv.Load(); err != nil {
-		return nil, fmt.Errorf("error loading .env file: %e", err)
-	}
+
 	if err := envconfig.Process("MenuService", cfg); err != nil {
 		return nil, fmt.Errorf("error processing MenuService env: %w", err)
 	}
